622d2d864c10d4c6ef1dced1b5910dad95c8cb46
Andrew Qu <qual1337@gmail.com>
2025-07-02 15:27:05 -0700

    middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { checkBotId } from "botid/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const botResult = await checkBotId();
+
+  if (botResult.isBot) {
+    return NextResponse.json(
+      { error: "Bot is not allowed to access this endpoint" },
+      { status: 401 }
+    );
+  }
+  // If no x-is-human header, just continue the request normally
+  // IMPORTANT:
+  //  Be sure to confirm the existance of the x-is-human header in the backend resource
+  //  Performing this check in the middleware allows for a bad actor to not pass
+  //  x-is-human to bypass the middleware check
+  //
+  //  Ensure that the header is present in the backend resource
+  return NextResponse.next();
+}
+
+// Configure which paths the middleware runs on
+export const config = {
+  matcher: [
+    {
+      source: "/(.*)",
+      has: [{ type: "header", key: "x-is-human" }],
+    },
+  ],
+};
